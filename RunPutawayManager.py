4ee48fc63dca9b141f1b9a7aae6e2161e11f053d
AndrewUnderton <64883561+AndrewUnderton@users.noreply.github.com>
2022-01-12 08:22:26 -0600

    Add files via upload

---
+++ RunPutawayManager.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import time
+import os
+import re
+
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from putaway_automation import putaway_automation
+from pendputaway_automation import pendputaway_automation
+from login_automation import login_automation
+from transfer_automation import transfer_automation
+
+# Get current working directory (relative file path)
+relative_path = os.getcwd()
+
+receiving_url = 'https://w16kcst2.int.hp.com/Rec/Putaway?boxID='
+
+# Initiate Chrome driver
+s=Service(relative_path + '\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+# Run through the local login_automation module.
+login_automation(s,driver)
+
+# Ask for the pallet number and location the user wants to migrate the units towards.
+new_pallet = input("Pallet # (If you want to generate one, type 'generate'): ")
+new_location = input('Pallet Location: ')
+
+if new_pallet.lower() == 'generate':
+    generate_bool = True
+    print("New pallet number will be generated.")
+else:
+    generate_bool = False
+    pass
+
+# Turn on or off "putaway" status reporting and updating.
+report_input = True
+while report_input == True:
+    report_putaway_ask = input("Would you like to have the putaway status of units reported (yes/no)? ")
+    report_putaway_ask = report_putaway_ask.lower()
+    report_putaway = False
+    if report_putaway_ask == 'yes':
+        report_putaway = True
+        print("Putaway status will be shown.\nIf any are in Putaway or Pendputaway status, you'll be able to fix them later in this program")
+        report_input = False
+    elif report_putaway_ask == 'no':
+        print('Putaway status will not be shown or corrected.')
+        report_input = False
+    else:
+        print('Invalid input, try again.')
+
+print('Enter "quit" to continue to the next step or "remove" to delete the last entry')        
+active_input = True
+boxids_and_info=[]
+# regular expression that, when used, removes everything but digits from the input string.
+recompile = re.compile("[^\d]")
+blank_dict = {'Box ID':'','Prev Pallet':'','New Pallet':'','Prev Location':'','New Location':'','Prev Status':'', 'New Status':''}
+# Sort of convoluted way of making a new dictionary with the correct Box ID.
+# Might be better to use classes, but I'm not familiar enough with them right now.
+# Next iteration might use classes, though.
+# UPDATE: Nevermind, the dictionaries work great I'm not changing them.
+while active_input:
+    boxid = input ('Box ID: ')
+    if boxid.lower() == "quit":
+        active_input = False
+    elif boxid.lower() == "remove":
+        del boxids_and_info[-1]
+    else:
+        # Make a copy of the blank dictionary and change the Box ID entry to the user input.
+        # Automatically removes anything but numbers from boxid using the aforementioned regular expressions.
+        new_dict = blank_dict.copy()
+        new_dict['Box ID'] = recompile.sub('', boxid)
+        boxids_and_info.append(new_dict)    
+
+# Runs the local transfer_automation module.
+transfer_automation(boxids_and_info, report_putaway, new_pallet, new_location, s, driver,generate_bool)
+
+# Change status to putaway and update boxids_and_info with the new status.
+if report_putaway == True:
+    for dict_index in boxids_and_info:
+        if dict_index['Prev Status'].lower() == 'putawayready':
+            putaway_automation(dict_index['Box ID'],s,driver)
+            dict_index['New Status'] = 'Putaway'
+        elif dict_index['Prev Status'].lower() == 'pendputaway':
+            pendputaway_automation(dict_index['Box ID'],new_pallet,new_location,s,driver)
+            putaway_automation(dict_index['Box ID'],s,driver)
+            dict_index['New Status'] = 'Putaway'
+
+# Close Chrome instance.
+driver.close()
+
+# Get the date and time and add it to the beginning of the list of Box IDs and their info.
+datetime_of_execution = time.asctime()
+boxids_and_info.insert(0,{'Time Executed':datetime_of_execution})
+# Append the list of units from this session to the text file specified
+data_towards_csv = pd.DataFrame(boxids_and_info)
+data_towards_csv.to_csv(relative_path + '\Reports\PutawayReference.txt', mode='a', index=None)
+print('Finished. Report saved to ' + str(relative_path) + '\Reports\PutawayReference.txt')
+
+
+# In[ ]:
+
+
+
+
---
+++ pendputaway_automation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def pendputaway_automation(boxid,new_pallet,new_location,s,driver):
+    """Handles the Putaway Receiving portion of putting a unit away. Specifically Pendputaway status."""
+    # Make the Pendputaway URL and go to it.
+    pend_url_prefix = ('https://w16kcst2.int.hp.com/Rec/Putaway?status=7&projectID=&boxID=')
+    pend_url = pend_url_prefix + str(boxid)
+    driver.get(pend_url)
+    
+    settings_button = driver.find_element(By.CLASS_NAME, 'mdi-pencil-outline')
+    settings_button.click()
+    # Find the textboxes for filling in location and pallet as well as the save button.
+    location_field = driver.find_element(By.ID, 'SingleEdit_MDM_Location_LocationNumber')
+    pallet_field = driver.find_element(By.ID, 'txtPalletID')
+    save_button = driver.find_element(By.ID, 'btnSavePendPutaway')
+    # Fill in the textboxes and save the information.
+    location_field.clear()
+    location_field.send_keys(new_location)
+    pallet_field.clear()
+    pallet_field.send_keys(new_pallet)
+    save_button.click()
+    # Wait til the new information saves (or time out after 60 seconds)
+    try:
+        WebDriverWait(driver, 60).until(EC.url_to_be('https://w16kcst2.int.hp.com/Rec/Putaway?firstload=True'))
+    except:
+        print("This has taken to long")
+        input("Press ENTER to continue once the page has loaded (or quit and restart from " + grab_id + "): ")
+
